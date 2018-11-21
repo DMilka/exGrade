068a7c5068a6fa5fdbee7e4ed58002faaebaaddb
DawidMilka <dawid.milka@interia.com>
2018-11-21 23:42:29 +0100

    Subject feature

--- app.js
+++ app.js
@@ -2,16 +2,25 @@ const gradeController = (function() {
   let Semester = function(id, name) {
     this.id = id;
     this.name = name;
+    this.subjects = [];
   };
 
-  let Subject = function(id, semesterID, type) {
+  let Subject = function(id, name, type) {
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.grades = [];
+  };
 
+  let Grade = function(id, name, value, weightValue) {
+    this.id = id;
+    this.name = name;
+    this.value =  value;
+    this.weightValue = weightValue;
   };
 
   let data = {
-    semesters: [],
-    subjects: [],
-    grades: []
+    semesters: []
   }
 
   return {
@@ -45,6 +54,10 @@ const gradeController = (function() {
       }
     },
 
+    showSemester: function(id) {
+      return data.semesters[id];
+    },
+
     test: function() {
       return data;
     }
@@ -59,15 +72,22 @@ const UIController = (function() {
     semester: '.semester',
     subject: '.subject',
     FormSemester: '.form_semester',
+    SemesterForm: '#add-semester',
+    SubjectForm: '#add-subject',
     addSemesterBtn: '#addSemester',
-    closeSemesterForm: '.close_button',
+    closeSemesterForm: '#close-semester-form-btn',
+    closeSubjectForm: '#close-subject-form-btn',
     semesterFormInput: '#semester_name',
     confirmAddSemesterBtn: '.add_button',
     semesterList: '#semester_list',
-    semesterDeleteBtn: '#semester_delete'
+    semesterDeleteBtn: '#semester_delete',
+    semesterName: '#semester-name',
+    subjectsList: '#subjects-list',
+    semesterCloseBtn: '#semester-close-btn',
+    addSubjectBtn: '#addSubject',
+    subjectSelect: '.select-select',
+    weightInput: '#weight-input'
   }
-
-
 
   return {
     getDOMstrings: function() {
@@ -94,9 +114,35 @@ const UIController = (function() {
     deleteSemesterFromUI: function(element) {
       let el = document.getElementById(element);
       el.parentNode.removeChild(el);
+    },
+
+    showSemesterInUI: function(items) {
+      let semesterName, html, newHtml;
+
+      document.querySelector(DOMstrings.semester).classList.remove('hidden');
+      document.querySelector(DOMstrings.main).classList.add('hidden');
+
+      semesterName = DOMstrings.semesterName;
+      document.querySelector(semesterName).textContent = items.name
+
+      for(let i = 0; i < items.subjects.length; i++) {
+        html = '<div class="container__listItem"><div class="container__listItem--name">%subjectName%</div><button class="container__listItem--btn" id="semester_delete">x</button></div>';
+        newHtml = html.replace('%subjectName%', items.subjects[i]['name']);
+        document.querySelector(DOMstrings.subjectsList).insertAdjacentElement('beforeend', newHtml);
+      }
+    },
+
+    closeSemesterInUI: function() {
+      let htmlToDestroy;
+
+      htmlToDestroy = DOMstrings.subjectsList;
+      while(htmlToDestroy.firstChild) {
+        htmlToDestroy.removeChild(htmlToDestroy.firstChild);
+      }
+
+      document.querySelector(DOMstrings.semester).classList.add('hidden');
+      document.querySelector(DOMstrings.main).classList.remove('hidden');
     }
-
-
   }
 })();
 
@@ -104,30 +150,81 @@ const controller = (function(gradeCtrl, UICtrl) {
   let setupEventListeners = function() {
     let DOM = UICtrl.getDOMstrings();
 
-    // Otwieranie formularza z dodawaniem semestru
-    document.querySelector(DOM.addSemesterBtn).addEventListener('click', function() {
-      document.querySelector(DOM.FormSemester).classList.remove('hidden');
-    });
+    /**********************************
+      Formularz dodawania semestru
 
-    // Zamykanie formularza z dodawaniem semestru
-    document.querySelector(DOM.closeSemesterForm).addEventListener('click', function() {
-      document.querySelector(DOM.FormSemester).classList.add('hidden');
-    });
+    **********************************/
+        //Otwieranie formularza z dodawaniem semestru
+        document.querySelector(DOM.addSemesterBtn).addEventListener('click', function() {
+          document.querySelector(DOM.FormSemester).classList.remove('hidden');
+          document.querySelector(DOM.SemesterForm).classList.remove('hidden');
+        });
 
-    // Zatwierdzenie formularza dodania semestru
-    document.querySelector(DOM.confirmAddSemesterBtn).addEventListener('click', function() {
-      addSemester();
-      document.querySelector(DOM.FormSemester).classList.add('hidden');
-    });
+        // Zamykanie formularza z dodawaniem semestru
+        document.querySelector(DOM.closeSemesterForm).addEventListener('click', function() {
+          document.querySelector(DOM.FormSemester).classList.add('hidden');
+          document.querySelector(DOM.SemesterForm).classList.add('hidden');
+        });
 
-    // Usuniecie semestru
-    document.querySelector(DOM.semesterList).addEventListener('click', deleteSemester);
+        // Zatwierdzenie formularza dodania semestru
+        document.querySelector(DOM.confirmAddSemesterBtn).addEventListener('click', function() {
+          addSemester();
+          document.querySelector(DOM.FormSemester).classList.add('hidden');
+        });
 
-    // Otwarcie okna z zawartoscia wybranego semestru
-    document.querySelector(DOM.semesterList).addEventListener('click', function(e) {
-      let item = e.target.parentNode;
-      console.log(item);
-    });
+        // Usuniecie semestru
+        document.querySelector(DOM.semesterList).addEventListener('click', deleteSemester);
+
+        // Otwarcie okna z zawartoscia wybranego semestru
+        document.querySelector(DOM.semesterList).addEventListener('click', function(e) {
+          let item, dividedItem, dividedID, semester;
+
+          item = e.target.parentNode.id;
+          dividedItem = item.split('-');
+          dividedID = parseInt(dividedItem[1]);
+          if (!isNaN(dividedID)) {
+            // 1. Pobranie danych z struktury danych
+            semester = gradeCtrl.showSemester(dividedID);
+
+            // 2. Wstawienie danych w odpowiednie pola oraz wygenerowanie listy z przedmiotami
+            UICtrl.showSemesterInUI(semester);
+          }
+
+        });
+
+        document.querySelector(DOM.semesterCloseBtn).addEventListener('click', UICtrl.closeSemesterInUI);
+    /**********************************
+      Formularz dodawania przedmiotu
+
+    **********************************/
+        //Otwieranie formularza z dodawaniem przedmiotu
+        document.querySelector(DOM.addSubjectBtn).addEventListener('click', function() {
+          document.querySelector(DOM.FormSemester).classList.remove('hidden');
+          document.querySelector(DOM.SubjectForm).classList.remove('hidden');
+        });
+
+        // Zamykanie formularza z dodawaniem przedmiotu
+        document.querySelector(DOM.closeSubjectForm).addEventListener('click', function() {
+          document.querySelector(DOM.FormSemester).classList.add('hidden');
+          document.querySelector(DOM.SubjectForm).classList.add('hidden');
+        });
+
+        // Zmiana typu przedmiotu
+        document.querySelector(DOM.subjectSelect).addEventListener('change', function(e) {
+          let selectValue, weightInput;
+
+          selectValue = e.target.value;
+          weightInput = document.querySelector(DOM.weightInput);
+
+          if(selectValue === 'weightAvg') {
+            weightInput.classList.remove('hidden');
+          } else {
+            if(!weightInput.classList.contains('hidden')) {
+              weightInput.classList.add('hidden');
+            }
+          }
+        });
+
 
   };
 
@@ -164,10 +261,19 @@ const controller = (function(gradeCtrl, UICtrl) {
       // 2. Usunięcie zawartości z UI
       UICtrl.deleteSemesterFromUI(itemToDeleteID);
     }
+  };
 
+  let addSubject = function() {
+    let inputs
+    // 1. Pobranie zawartosci z inputow
+    inputs = UI.getValueFromSubjectInputs();
 
+    // 2. Walidacja i dodanie do struktury danych nowego przedmiotu
 
-  }
+    // 3. Usunięcie zawartości z inputa
+
+    // 4. Dodanie zawartości do widoku
+  };
 
   return {
     init: function() {
